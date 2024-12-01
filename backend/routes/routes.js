bc6dca5722dbde1a636b5071e402b150304029f6
Sneha Gupta <156881708+Snehagupta00@users.noreply.github.com>
2024-12-01 18:37:16 +0530

    Update routes.js

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
-
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/login.html"));
+});
+
 router.get("/home.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/home.html"));
+  res.sendFile(path.join(__dirname, "../../public/home.html"));
 });
 
 module.exports = router;
